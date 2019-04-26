f04346c9d1d58568fd1337ff4cb4fd15b0f49116
vocafeuvre <hirafura96@gmail.com>
2019-04-26 23:51:09 +0800

    Fixed image in Listing List

--- src/components/ListingList/index.js
+++ src/components/ListingList/index.js
@@ -1,39 +1,38 @@
 /* eslint-disable camelcase */
 import React from 'react'
-import {Card, Image, Label, Loader, Segment} from 'semantic-ui-react'
-import Img from 'gatsby-image'
+import {Card, Image, Label, Segment} from 'semantic-ui-react'
 import {Link} from 'gatsby'
 
 const mapListingsToItems = listings =>
   listings.map(
-    ({name, id, price, mainImage}) => {
+    ({name, id, price, priceUom, mainImage, isNew}) => {
       return {
         as: Link,
-        // to: `/listing/${id}/`,
+        to: `/listing/${id}/`,
         childKey: id,
         image: (
-          <Image>
-            {/* {isNew ? (
+          <Image
+            src={mainImage.src}
+            fluid>
+            {isNew ? (
               <Label color="red" ribbon style={{zIndex: '1'}}>
                 New!
               </Label>
-            ) : null} */}
-            <Img
-              fluid={mainImage}
-              alt={name}
-            />
+            ) : null}
           </Image>
         ),
         header: name,
-        meta: <Card.Meta style={{color: 'dimgray'}}>{price}</Card.Meta>,
+        meta: <Card.Meta style={{color: 'dimgray'}}>{`${priceUom} ${price}`}</Card.Meta>,
       }
     },
   )
 
 export default ({listings, loading}) => (
-    <Segment>
-        <Loader active={loading} />
-        { loading ? <></> : <Card.Group items={mapListingsToItems(listings)} itemsPerRow={2} stackable /> }
+    <Segment basic loading={loading} style={{minHeight: "100px"}}>
+        { loading ? 
+            <></>  : 
+            <Card.Group items={mapListingsToItems(listings)} itemsPerRow={2} stackable /> 
+        }
     </Segment>
   
 )
