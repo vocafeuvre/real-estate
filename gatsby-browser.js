6ad79d8e837477c72323681deca259ef0418fd9b
vocafeuvre <hirafura96@gmail.com>
2019-05-01 01:27:15 +0800

    Added css styling for carousel

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import wrapWithProvider from './wrap-with-provider'
 import './src/styles/global.css'
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 // eslint-disable-next-line import/prefer-default-export
 export const wrapRootElement = wrapWithProvider
