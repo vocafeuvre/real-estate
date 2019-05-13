c3e6a9faf7648c40304abaf656c0cf86bf0972f1
vocafeuvre <hirafura96@gmail.com>
2019-05-13 21:02:43 +0800

    Added message if no listings are found

--- src/components/ListingList/index.js
+++ src/components/ListingList/index.js
@@ -1,38 +1,53 @@
 /* eslint-disable camelcase */
-import React from 'react'
-import {Card, Image, Label, Segment} from 'semantic-ui-react'
-import {Link} from 'gatsby'
+import React from "react"
+import { Card, Image, Label, Segment, Message } from "semantic-ui-react"
+import { Link } from "gatsby"
+
+const NO_LISTINGS_YET = "You haven't created any listings yet."
 
 const mapListingsToItems = listings =>
-  listings.map(
-    ({name, id, price, priceUom, mainImage, isNew}) => {
-      return {
-        as: Link,
-        to: `/listings/${id}/`,
-        childKey: id,
-        image: (
-          <Image
-            src={mainImage.src}
-            fluid>
-            {isNew ? (
-              <Label color="red" ribbon style={{zIndex: '1'}}>
-                New!
-              </Label>
-            ) : null}
-          </Image>
-        ),
-        header: name,
-        meta: <Card.Meta style={{color: 'dimgray'}}>{`${priceUom} ${price}`}</Card.Meta>,
-      }
-    },
-  )
-
-export default ({listings, loading}) => (
-    <Segment basic loading={loading} style={{minHeight: "100px"}}>
-        { loading ? 
-            <></>  : 
-            <Card.Group items={mapListingsToItems(listings)} itemsPerRow={2} stackable /> 
+    listings.map(({ name, id, price, priceUom, mainImage, isNew }) => {
+        return {
+            as: Link,
+            to: `/listings/${id}/`,
+            childKey: id,
+            image: (
+                <Image src={mainImage.src} fluid>
+                    {isNew ? (
+                        <Label color="red" ribbon style={{ zIndex: "1" }}>
+                            New!
+                        </Label>
+                    ) : null}
+                </Image>
+            ),
+            header: name,
+            meta: (
+                <Card.Meta style={{ color: "dimgray" }}>{`${priceUom} ${Number(
+                    price
+                )
+                    .toFixed(2)
+                    .replace(/\d(?=(\d{3})+\.)/g, "$&,")}`}</Card.Meta>
+            ),
         }
-    </Segment>
-  
-)
+    })
+
+export default ({ listings, loading }) => {
+    return (
+        <Segment basic loading={loading} style={{ minHeight: "100px" }}>
+            {loading ? (
+                <></>
+            ) : listings ? (
+                <Card.Group
+                    items={mapListingsToItems(listings)}
+                    itemsPerRow={2}
+                    stackable
+                />
+            ) : (
+                <Message info>
+                    <Message.Header>No Listings Yet</Message.Header>
+                    {NO_LISTINGS_YET}
+                </Message>
+            )}
+        </Segment>
+    )
+}
