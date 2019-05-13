2308b4968375c1aa46ce8f83731a633f8a539cad
vocafeuvre <hirafura96@gmail.com>
2019-05-13 20:58:03 +0800

    Added firebase storage to app initialization

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,14 @@
 import firebase from "@firebase/app"
 import "@firebase/auth"
 import "@firebase/firestore"
+import "@firebase/storage"
 
 const firebaseConfig = {
   apiKey: process.env.GATSBY_FIREBASE_API_KEY,
   authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
-  projectId: process.env.GATSBY_FIREBASE_PROJ_ID
+  projectId: process.env.GATSBY_FIREBASE_PROJ_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_BUCKET
 }
 
 firebase.initializeApp(firebaseConfig);
